0446259951fcb506414677003040e8bd86dd3a44
Aadil M. Alli <chainedraptor@gmail.com>
2025-10-15 20:33:24 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-black h-screen w-full flex items-center justify-center">
-      <div className="text-white font-bold text-5xl font-mirage animate-spin3d p-4">VLMT</div>
+      <div className="text-white font-bold text-8xl font-mirage animate-spin3d p-4">VLMT</div>
     </div>
   )
 }
